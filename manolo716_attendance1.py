c630d75345c1ebb8d07eb6b532b5e9d658dc40ed
Jihoon Kim <jihoon0716@gmail.com>
2016-05-29 22:44:26 +0900

    Update manolo716_attendance1.py

--- manolo716_attendance1.py
+++ manolo716_attendance1.py
@@ -1,14 +1,15 @@
-
+#HW1 
 student = ["차경묵","김지훈","손형우","남시욱"]
 attendances = []
-
+#날짜,이름 출석부에 입력하기
 def attend (date, name):
     if name in student:
         attendances.append(date)
         attendances.append(name)
+    #대출 방지
     else:
         print("대출은 안됩니다.")
 
-
+#수강생 추가하기
 def adder (name):
     student.append(name)
