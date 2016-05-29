c5e1198b6b174565be796776fc437236c9a55006
Jihoon Kim <jihoon0716@gmail.com>
2016-05-29 22:45:32 +0900

    Update manolo716_attendance2.py

--- manolo716_attendance2.py
+++ manolo716_attendance2.py
@@ -1,10 +1,12 @@
+#Hw2
 students = []
 attendances =[]
+#이름 students에 저장하기
 class Student:
     def __init__(self,name):
         self.name = name
         students.append(name)
-
+#출석확인하고, 명단보여주기
 class Attendance(Student):
     def attend(name):
         attendances.append(name)
