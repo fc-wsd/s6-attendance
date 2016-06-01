cb5a2f0341858fee6c1329f17791e154dc76d3c1
hskim <nussland@gmail.com>
2016-06-01 13:30:16 +0900

    출석부 만들기2 요구사항 반영 수정

--- nussland_attendance2.py
+++ nussland_attendance2.py
@@ -7,13 +7,13 @@
 import time
 
 class Student:
-    students = ['김효성', '박건희', '손종국', '김재현', '홍창우', '임재민', '김지훈', '박문수', '박두철', '설석주', '박근일' ,'김수석']
+    name = ''
 
 class Attendance:
     attendances = {}
 
-    def attend(self, name):
-        self.attendances[name] = time.strftime("%Y-%m-%d %H:%M:%S")
+    def attend(self, studentObj):
+        self.attendances[studentObj.name] = time.strftime("%Y-%m-%d %H:%M:%S")
 
     def list(self):
         print("[출석자 명단(이름:일시)]")
@@ -21,14 +21,16 @@ class Attendance:
             print(name +": "+self.attendances[name])
 
 def main():
+    students = ['김효성', '박건희', '손종국', '김재현', '홍창우', '임재민', '김지훈', '박문수', '박두철', '설석주', '박근일' ,'김수석']
     student = Student()
     attendance = Attendance()
 
     # 출석 처리
-    for name in student.students:
-        attendance.attend(name)
+    for name in students:
+        student.name = name
+        attendance.attend(student)
 
-    # 출석자 명단
+    # 출석자 명단출력
     attendance.list()
 
 if __name__ == "__main__":
